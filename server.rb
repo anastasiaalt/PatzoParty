3f5c6cd7f056ee42171f0ec9586cd298b2af4278
Anastasia Alt <anastasia.alt@gmail.com>
2015-12-16 13:28:18 -0500

    routing for log in and new users, new user sign up forms

--- server.rb
+++ server.rb
@@ -6,8 +6,45 @@ module App
 
   get "/" do
     @author = Author.find(session[:author_id]) if session[:author_id]
-    erb(:index)
+    @editor = Editor.find(session[:editor_id]) if session[:editor_id]
+    erb :index
   end
 
+  post "/sessions" do
+    author = Author.find_by({name: params[:name]}).try(:authenticate, params[:password])
+    editor = Editor.find_by({name: params[:name]}).try(:authenticate, params[:password])
+    if author
+      session[:author_id] = author.id
+      redirect to "/"
+    elsif
+      session[:editor_id] = editor.id
+      redirect to "/"
+    else
+      redirect to "/login"
+    end
+  end  
+
+  get '/login' do
+    erb :login
   end
+
+  get '/new_author' do
+    erb :new_author
+  end
+
+  post '/new_author' do
+    "Add new author to authors table"
+    redirect to "/"
+  end
+
+  get '/new_editor' do
+    erb :new_editor
+  end
+
+  post '/new_editor' do
+    "Add new editor to editors table"
+    redirect to "/"
+  end
+
+  end 
 end 
