5f677e83fb743880099066b19bdb5066188a5f90
Anastasia Alt <anastasia.alt@gmail.com>
2015-12-16 11:41:55 -0500

    new database name, attempting to re-seed with error

--- db/config.rb
+++ db/config.rb
@@ -2,7 +2,7 @@ require "active_record"
 
 ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
-  :database =>  'db/database.sqlite3'
+  :database =>  'db/themeparties.sqlite3'
 )
 
 Dir.glob("models/*.rb").each do |path|
