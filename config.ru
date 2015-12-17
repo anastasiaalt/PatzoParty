21f29072625d37bb78a9dc9bec92f34dc06c8202
Anastasia Alt <anastasia.alt@gmail.com>
2015-12-17 10:21:10 -0500

    login password troubleshooting

--- config.ru
+++ config.ru
@@ -2,7 +2,7 @@ require "pry"
 require "sinatra"
 require "bcrypt"
 
-require "db/config"
+require_relative "db/config"
 require_relative "server"
 
 run App::Server
--- models/category.rb
+++ models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
   belongs_to :article, class_name: "Article"
-  has_many foreign_key: "visited_id"
+  has_many :labels, foreign_key: "article_id"
 end
